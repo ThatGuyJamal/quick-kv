fab36c738841c1732a55cfe0ba4bfe0f394b8be7
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-10-04 21:43:35 -0400

    Create publish-check.sh

---
+++ scripts/publish-check.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Check if the current directory is a Rust crate
+if ! cargo metadata --no-deps --format-version 1 >/dev/null 2>&1; then
+  echo "Error: Not a Rust crate directory. Please run this script in your crate's root directory."
+  exit 1
+fi
+
+# Check if cargo fmt reports any formatting issues
+if ! cargo fmt --all -- --check; then
+  echo "Error: Formatting issues detected. Please run 'cargo fmt' to format your code."
+  exit 1
+fi
+
+# Check if clippy lints report any issues
+if ! cargo clippy --all -- -D warnings; then
+  echo "Error: Clippy lints detected issues. Please fix the reported warnings and errors."
+  exit 1
+fi
+
+# Generate documentation to check for issues
+if ! cargo doc --no-deps --all --release; then
+  echo "Error: Failed to generate documentation. Please fix any doc comments or build issues."
+  exit 1
+fi
+
+# Check if all doc tests pass
+if ! cargo test --doc --all; then
+  echo "Error: Some documentation tests failed. Please ensure doc comments are correct."
+  exit 1
+fi
+
+# Check if cargo publish would succeed (dry run)
+if ! cargo publish --dry-run; then
+  echo "Error: 'cargo publish' dry run failed. Please fix any issues reported."
+  exit 1
+fi
+
+echo "Everything looks good! You can proceed with 'cargo publish' to publish your crate."
+exit 0
