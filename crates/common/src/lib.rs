95aa842f2a453bc787c68bd3bb8646ad8ed43016
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2024-04-21 14:09:02 -0400

    Update lib.rs

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -10,3 +10,11 @@
 /// socket. `std::error::Error` is implemented for `parse::Error` which allows
 /// it to be converted to `Box<dyn std::error::Error>`.
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+/// A specialized `Result` type for the db operations.
+///
+/// This is defined as a convenience.
+pub type Result<T> = std::result::Result<T, Error>;
+
+// The normal result type in rust std lib.
+pub type StdResult<T, E> = std::result::Result<T, E>;
